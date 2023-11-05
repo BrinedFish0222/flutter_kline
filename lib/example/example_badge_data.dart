40e1ee13e3cb1bc8ddb48f5e47f053a2710896d8
BrinedFish0222 <75007756+BrinedFish0222@users.noreply.github.com>
2023-11-05 21:41:49 +0800

    调整代码

--- lib/example/example_badge_data.dart
+++ lib/example/example_badge_data.dart
@@ -2,48 +2,49 @@ import 'package:flutter/material.dart';
 import 'package:flutter_kline/vo/badge_chart_vo.dart';
 
 class ExampleBadgeData {
-
-  static BadgeChartVo badgeChartVo = BadgeChartVo(
-      data: []
-        ..length = 796
-        ..addAll([
-          BadgeChartData(
-            widget: LayoutBuilder(builder: (context, constraints) {
-              return Container(
-                color: Colors.red,
-                width: constraints.maxWidth,
-                height: constraints.maxHeight,
-                child: Center(
-                  child: Text(
-                    "买",
-                    style: TextStyle(
-                      fontSize: constraints.maxWidth / 2,
-                      color: Colors.white,
+  static BadgeChartVo get badgeChartVo {
+    return BadgeChartVo(
+        data: []
+          ..length = 796
+          ..addAll([
+            BadgeChartData(
+              widget: LayoutBuilder(builder: (context, constraints) {
+                return Container(
+                  color: Colors.red,
+                  width: constraints.maxWidth,
+                  height: constraints.maxHeight,
+                  child: Center(
+                    child: Text(
+                      "买",
+                      style: TextStyle(
+                        fontSize: constraints.maxWidth / 2,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              );
-            }),
-          ),
-          null,
-          null,
-          BadgeChartData(
-            widget: LayoutBuilder(builder: (context, constraints) {
-              return Container(
-                color: Colors.blue,
-                width: constraints.maxWidth,
-                height: constraints.maxHeight,
-                child: Center(
-                  child: Text(
-                    "卖",
-                    style: TextStyle(
-                      fontSize: constraints.maxWidth / 2,
-                      color: Colors.white,
+                );
+              }),
+            ),
+            null,
+            null,
+            BadgeChartData(
+              widget: LayoutBuilder(builder: (context, constraints) {
+                return Container(
+                  color: Colors.blue,
+                  width: constraints.maxWidth,
+                  height: constraints.maxHeight,
+                  child: Center(
+                    child: Text(
+                      "卖",
+                      style: TextStyle(
+                        fontSize: constraints.maxWidth / 2,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              );
-            }),
-          ),
-        ]));
+                );
+              }),
+            ),
+          ]));
+  }
 }
