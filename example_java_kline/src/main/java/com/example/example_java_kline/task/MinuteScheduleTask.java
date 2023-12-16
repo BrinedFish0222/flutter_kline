304a2539c7c50371ebf39e47c5950ab75698f6a7
BrinedFish0222 <2603981051@qq.com>
2023-12-16 17:24:41 +0800

    Java端 分时增加全量数据返回socket

--- example_java_kline/src/main/java/com/example/example_java_kline/task/MinuteScheduleTask.java
+++ example_java_kline/src/main/java/com/example/example_java_kline/task/MinuteScheduleTask.java
@@ -1,6 +1,5 @@
 package com.example.example_java_kline.task;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +12,24 @@ import com.example.example_java_kline.example.ExampleMinuteData;
 import com.example.example_java_kline.vo.ResponseResult;
 import com.example.example_java_kline.websocket.WebSocketTest;
 
-
+/**
+ * 分时定时任务
+ */
 @Configuration
 @EnableScheduling
 @EnableAsync
-public class SaticScheduleTask {
-    
+public class MinuteScheduleTask {
+
+    /**
+     * 分时 - 全部数据索引位置
+     */
+    private static int minuteAllIndex = 0;
+
+    /**
+     * 分时 - 单根数据
+     */
     @Scheduled(fixedRate=300)
-    private void configureTasks() {
+    private void minuteSingleTask() {
         // System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
         if (ExampleMinuteData.index >= ExampleMinuteData.lineData2.size()) {
             ExampleMinuteData.index = 0;
@@ -38,5 +47,35 @@ public class SaticScheduleTask {
 
         WebSocketTest.sendMessage(responseResult);
         ExampleMinuteData.index += 1;
+    }
+
+    /**
+     * 分时 - 全部数据
+     */
+    @Scheduled(fixedRate = 300)
+    private void minuteAllTask() {
+        // System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+        if (minuteAllIndex >= ExampleMinuteData.lineData2.size()) {
+            minuteAllIndex = 0;
+        }
+
+        List<List<Double>> sendDataList = new ArrayList<>();
+        for (int i = 0; i <= minuteAllIndex; ++i) {
+            List<Double> dataList = ExampleMinuteData.lineData2.get(i);
+            // 时间
+            Double date = dataList.get(8);
+            // 值
+            Double value = dataList.get(5);
+            List<Double> sendData = new ArrayList<>();
+            sendData.add(date);
+            sendData.add(value);
+            sendData.add(ExampleMinuteData.a1.get(i));
+            sendDataList.add(sendData);
+        }
+
+        ResponseResult<List<List<Double>>> responseResult = ResponseResult.success("minuteAll", sendDataList);
+
+        WebSocketTest.sendMessage(responseResult);
+        minuteAllIndex += 1;
     }
 }
