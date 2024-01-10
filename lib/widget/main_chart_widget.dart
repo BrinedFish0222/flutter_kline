f3994e59cd266c3e99924818916c93989955bd24
BrinedFish0222 <75007756+BrinedFish0222@users.noreply.github.com>
2024-01-10 22:10:07 +0800

    修复十字线左边值不准确问题

--- lib/widget/main_chart_widget.dart
+++ lib/widget/main_chart_widget.dart
@@ -137,16 +137,16 @@ class _MainChartWidgetState extends State<MainChartWidget> {
                       builder: (context, snapshot) {
                         Pair<double?, double?> selectedXY =
                             Pair(left: null, right: null);
-                        if (snapshot.data != null && !snapshot.data!.isNull()) {
-                          RenderBox renderBox = _chartKey.currentContext!
-                              .findRenderObject() as RenderBox;
 
+                        RenderBox renderBox = _chartKey.currentContext!
+                            .findRenderObject() as RenderBox;
+                        if (snapshot.data != null && !snapshot.data!.isNull()) {
                           Offset? selectedOffset =
                               snapshot.data == null || snapshot.data!.isNull()
                                   ? null
                                   : renderBox.globalToLocal(Offset(
                                       snapshot.data?.left ?? 0,
-                                      snapshot.data?.right ?? 0));
+                                      snapshot.data?.right ?? 0),);
                           selectedXY.left = selectedOffset?.dx;
                           selectedXY.right = selectedOffset?.dy;
                         }
@@ -154,7 +154,7 @@ class _MainChartWidgetState extends State<MainChartWidget> {
                         double? selectedHorizontalValue =
                             KlineUtil.computeSelectedHorizontalValue(
                                 maxMinValue: maxMinValue,
-                                height: widget.size.height,
+                                height: renderBox.size.height,
                                 selectedY: selectedXY.right);
 
                         return CustomPaint(
