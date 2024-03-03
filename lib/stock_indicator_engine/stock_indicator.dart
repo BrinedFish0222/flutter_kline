ac9ce34e2abfe227f437bb6d5c244f6e05e13326
BrinedFish0222 <2603981051@qq.com>
2024-03-03 08:36:28 +0800

    指标引擎 定义引擎使用规范

--- lib/stock_indicator_engine/stock_indicator.dart
+++ lib/stock_indicator_engine/stock_indicator.dart
@@ -1,17 +1,3 @@
-/// 指标
-class StockIndicator {
-  /// 参数
-  final List<StockIndicatorParameter> parameters;
-
-  /// 公式
-  final String formula;
-
-  const StockIndicator({
-    this.parameters = const [],
-    required this.formula,
-  });
-}
-
 /// 指标参数
 class StockIndicatorParameter {
   final String name;
--- lib/stock_indicator_engine/stock_indicator_engine.dart
+++ lib/stock_indicator_engine/stock_indicator_engine.dart
@@ -1,23 +1,17 @@
-
 import 'package:flutter_kline/stock_indicator_engine/stock_indicator.dart';
 
 import '../vo/base_chart_vo.dart';
 
 /// 股票指标引擎
 class StockIndicatorEngine {
-  static final StockIndicatorEngine _instance = StockIndicatorEngine._internal();
+  StockIndicatorEngine({
+    required this.formula,
+    this.parameters = const [],
+  });
 
-  StockIndicatorEngine._internal();
+  /// 公式
+  final String formula;
 
-  factory StockIndicatorEngine() {
-    return _instance;
-  }
-
-  List<BaseChartVo> run(StockIndicator stockIndicator) {
-    // TODO 尚未实现
-    throw Exception("尚未实现");
-  }
-
+  /// 参数
+  final List<StockIndicatorParameter> parameters;
 }
-
-
