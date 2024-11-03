8a913b9a6059b5dcaf546af62c66e7d96101ab23
Brinedfish <26039@qq.com>
2024-11-03 18:53:29 +0800

    画线-圆形：定义数据规范

---
+++ lib/chart/circle_chart.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+
+import 'package:flutter/src/painting/edge_insets.dart';
+
+import 'package:flutter_kline/common/chart_show_data_item_vo.dart';
+
+import 'package:flutter_kline/common/pair.dart';
+import 'package:flutter_kline/draw/draw_circle_chart.dart';
+
+import '../common/utils/kline_num_util.dart';
+import 'base_chart.dart';
+
+class CircleChart<E> extends BaseChart<CircleChartData<E>> {
+  CircleChart({
+    required super.id,
+    required super.name,
+    required super.maxValue,
+    required super.minValue,
+    required super.data,
+    super.drawChartType = DrawCircleChart.drawKey,
+  });
+
+  @override
+  CircleChart<E> copy() {
+    return CircleChart(
+      id: id,
+      name: name,
+      maxValue: maxValue,
+      minValue: minValue,
+      data: data.sublist(0),
+    );
+  }
+
+  @override
+  int get dataLength => data.length;
+
+  @override
+  double? getDataMaxValueByIndex(int index) {
+    if (index >= dataLength) {
+      return null;
+    }
+
+    return data[index]?.value;
+  }
+
+  @override
+  Pair<double, double> getMaxMinData() {
+    if (data.isEmpty) {
+      return Pair.defaultMaxMinValue;
+    }
+
+    var valueList =
+        data.whereType<CircleChartData>().map((e) => e.value).toList();
+    Pair<double, double> maxMinValue =
+        KlineNumUtil.maxMinValueDouble(valueList);
+    return maxMinValue;
+  }
+
+  @override
+  List<ChartShowDataItemVo?>? getSelectedShowData() {
+    return data
+        .map((e) => ChartShowDataItemVo(name: name ?? '', value: e?.value))
+        .toList();
+  }
+
+  @override
+  void paint({
+    required Canvas canvas,
+    required Size size,
+    required Pair<double, double> maxMinValue,
+    required double pointWidth,
+    required double pointGap,
+    required EdgeInsets padding,
+  }) {
+    // TODO: implement paint
+  }
+}
+
+class CircleChartData<E> extends BaseChartData<E> {
+  /// 最左边顶点数值
+  final double value;
+
+  /// 左边和右边的间隔数
+  final int spaceNumber;
+
+  CircleChartData({
+    required super.id,
+    required super.dateTime,
+    super.extrasData,
+    required this.value,
+    required this.spaceNumber,
+  });
+}
