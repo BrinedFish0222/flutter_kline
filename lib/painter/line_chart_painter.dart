a84131c5fde1dd7c0d71d91924e40d09b2408881
Brinedfish <26039@qq.com>
2024-11-10 15:38:17 +0800

    画线：更换线图实现规范，实现DrawChartPainter规范

--- lib/painter/line_chart_painter.dart
+++ lib/painter/line_chart_painter.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kline/common/pair.dart';
+import 'package:flutter_kline/draw/draw_chart.dart';
 
 import '../chart/line_chart.dart';
 import '../common/utils/kline_num_util.dart';
@@ -7,18 +8,20 @@ import '../common/utils/kline_util.dart';
 import 'gradient_chart_painter.dart';
 
 /// 折线图
-class LineChartPainter extends CustomPainter {
+class LineChartPainter extends CustomPainter with DrawChartPainter {
   final LineChart lineChartData;
   final double? pointWidth;
   final double pointGap;
   final Pair<double, double>? maxMinValue;
 
-  const LineChartPainter({
+  LineChartPainter({
     required this.lineChartData,
     this.pointWidth,
     this.pointGap = 0,
     this.maxMinValue,
   });
+
+  final List<Path> _paths = [];
 
   /// 初始化：最大最小值。
   Pair<double, double> _initMaxMinValue() {
@@ -50,44 +53,38 @@ class LineChartPainter extends CustomPainter {
 
     paint.color = lineChartData.color;
 
-    var convertDataList = KlineUtil.convertDataToChartData(
-      lineChartData.data.map((e) {
-        if (e?.value == 0) {
-          return null;
-        }
-
-        return e?.value;
-      }).toList(),
+    var values = lineChartData.data.map((e) => e?.value).toList();
+    List<double?> dys = KlineUtil.convertDataToChartData(
+      values,
       size.height,
       maxMinValue: maxMinValue,
     );
 
-    double? lastX;
-    double? lastY;
-    for (int j = 0; j < convertDataList.length; j++) {
-      double? data = convertDataList[j];
-      if (data == null) {
+    Path path = Path();
+    bool hasData = false;
+    for (int j = 0; j < dys.length; j++) {
+      double? dy = dys[j];
+      if (dy == null) {
+        if (hasData) break;
         continue;
       }
 
-      lastX ??= KlineUtil.computeXAxis(
+      var dx = KlineUtil.computeXAxis(
           index: j, pointWidth: pointWidth, pointGap: pointGap);
-      lastY ??= data;
-
-      double x = KlineUtil.computeXAxis(
-          index: j, pointWidth: pointWidth, pointGap: pointGap);
-      double y = data;
-
-      // KlineUtil.logd('LineChartPainter lastX $lastX, lastY $lastY, x $x, y $y, j $j, data $data, originData: ${lineChartData.data[j]?.value}');
-      canvas.drawLine(Offset(lastX, lastY), Offset(x, y), paint);
-      lastX = x;
-      lastY = y;
+      if (hasData) {
+        path.lineTo(dx, dy);
+      } else {
+        path.moveTo(dx, dy);
+      }
+      hasData = true;
     }
+    _paths.add(path);
+    canvas.drawPath(path, paint);
 
     if (lineChartData.gradient != null) {
       GradientChartPainter(
         gradient: lineChartData.gradient!,
-        heightList: convertDataList,
+        heightList: dys,
         pointWidth: pointWidth,
         pointGap: pointGap,
       ).paint(canvas, size);
@@ -98,4 +95,7 @@ class LineChartPainter extends CustomPainter {
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
     return true;
   }
+
+  @override
+  List<Path> get chartPaths => _paths;
 }
